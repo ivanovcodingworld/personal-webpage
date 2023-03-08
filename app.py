07b938206f35fc59933b956a69bf555b101634f1
Ivan Ivanov <127295712+ivanovcodingworld@users.noreply.github.com>
2023-03-08 13:57:44 +0000

    Added style.css and images folder

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+jobs = [1,2,3,4,5]
+
 @app.route("/")
 def hello_world():
-  return "Hello World"
+  return render_template("home.html", jobs = jobs)
 
+  
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug = True)
